4b37acc7d3ecb1a23b2d83576785aa7b0b5fb69e
preciousLan <olanrewaju.faj@gmail.com>
2026-01-31 11:38:04 +0100

    Replace getMe with getUserById function

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,8 +1,17 @@
-exports.getMe = (req, res) => {
-	res.status(200).json({
-		status: 'success',
-		data: {
-			user: req.user,
-		},
-	});
+export const getUserById = async (req, res) => {
+	try {
+		const userId = req.user._id;
+		const user = await User.findById(userId);
+		if (!user) {
+			return res.status(404).json({ message: 'User not found' });
+		}
+		//return User
+		user.password = undefined; //hide password
+		return res.status(200).json({
+			message: 'User fetched successfully',
+			user,
+		});
+	} catch (error) {
+		return res.status(400).json({ message: error.message });
+	}
 };
