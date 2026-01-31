a8783adb25aded642d674e48569224c00c38ced5
preciousLan <olanrewaju.faj@gmail.com>
2026-01-31 11:41:36 +0100

    Update route to get user by ID

--- routes/userRouter.js
+++ routes/userRouter.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const authController = require('../controllers/authController');
 const userController = require('../controllers/userController');
 
-router.get('/me', authController.protect, userController.getMe );
+router.get('/me', authController.protect, userController.getUserById );
 router.post('/signup', authController.signup);
 router.post('/login', authController.login);
 
